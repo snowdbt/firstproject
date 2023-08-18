59665644a7c04fd261b2d6c1482a8c75607fe461
Harish Pabbisetty <pabbisetty.hm@gmail.com>
2023-08-18 02:42:21 +0000

    a

--- models/copy-two.sql
+++ models/copy-two.sql
@@ -1,3 +1,3 @@
--- select * from DBT.CONNECTFASTER.PERSONS;
-INSERT INTO CONNECTFASTER.Persons (PersonID, LastName,FirstName, Address, City)
+ select * from DBT.CONNECTFASTER.PERSONS;
+--INSERT INTO CONNECTFASTER.Persons (PersonID, LastName,FirstName, Address, City)
 VALUES ('2', 'Tom B. Erichsen','single','Skagen 21', 'Stavanger');
