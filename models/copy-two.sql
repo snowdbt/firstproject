71d479d4d104e0b4923002a012434888f3cb3e7c
Harish Pabbisetty <pabbisetty.hm@gmail.com>
2023-08-18 02:49:51 +0000

    added select into query.

--- models/copy-two.sql
+++ models/copy-two.sql
@@ -1,3 +1,13 @@
- select * from DBT.CONNECTFASTER.PERSONS;
---INSERT INTO CONNECTFASTER.Persons (PersonID, LastName,FirstName, Address, City)
-VALUES ('2', 'Tom B. Erichsen','single','Skagen 21', 'Stavanger');
+--select * from DBT.CONNECTFASTER.PERSONS;
+--INSERT INTO CONNECTFASTER.Persons (PersonID, LastName,FirstName, Address, City) VALUES ('3', 'Tom B. Erichsen','single','Skagen 21', 'Stavanger');
+
+INSERT INTO DBT.CONNECTFASTER.Persons_bkp select * from DBT.CONNECTFASTER.Persons limit 1; 
+
+-- Created backup table
+/*CREATE TABLE DBT.CONNECTFASTER.Persons_bkp (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);*/
