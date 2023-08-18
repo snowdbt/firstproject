8a3e7bb559d714ff98c401545bbfebff1df73e69
Harish Pabbisetty <pabbisetty.hm@gmail.com>
2023-08-18 02:40:49 +0000

    commit new files

--- models/copy-two.sql
+++ models/copy-two.sql
@@ -1,4 +1,3 @@
---select * from DBT.CONNECTFASTER.PERSONS;
-
+-- select * from DBT.CONNECTFASTER.PERSONS;
 INSERT INTO CONNECTFASTER.Persons (PersonID, LastName,FirstName, Address, City)
 VALUES ('2', 'Tom B. Erichsen','single','Skagen 21', 'Stavanger');
--- models/example/sample.sql
+++ models/example/sample.sql
@@ -1,20 +1,21 @@
+{{ config(materialized="incremental", unique_key="user_id") }}
 
-{{ config(materialized='incremental', unique_key='user_id') }}
+with
+    users_aggregated as (
 
-with users_aggregated as (
+        select
+            user_id,
+            min(event_time) as first_event_time,
+            max(event_time) as last_event_time,
+            count(*) as count_total_events
 
-    select
-        user_id,
-        min(event_time) as first_event_time,
-        max(event_time) as last_event_time,
-        count(*) as count_total_events
+        from {{ ref("users") }}
+        group by 1
 
-    from {{ ref('users') }}
-    group by 1
+    )
 
-)
-
-select *,
+select
+    *,
     -- Inject the run id if present, otherwise use "manual"
     '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}' as _audit_run_id
 
