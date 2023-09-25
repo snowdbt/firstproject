de6bb5043ce01d00ebb235bc9e49603097d0793c
Harish Pabbisetty <pabbisetty.hm@gmail.com>
2023-09-25 18:28:35 +0000

    commit to Github

--- models/copy-two.sql
+++ models/copy-two.sql
@@ -1,9 +1,7 @@
---select * from dbt.connectfaster.persons
-;
--- INSERT INTO CONNECTFASTER.Persons (PersonID, LastName,FirstName, Address, City)
--- VALUES ('3', 'Tom B. Erichsen','single','Skagen 21', 'Stavanger');
-INSERT INTO DBT.CONNECTFASTER.Persons_bkp select * from DBT.CONNECTFASTER.Persons limit 1; 
-
+select *from dbt.connectfaster.persons;
+--INSERT INTO CONNECTFASTER.Persons (PersonID, LastName,FirstName, Address, City)  VALUES ('3', 'Tom B. Erichsen','single','Skagen 21', 'Stavanger');
+-- INSERT INTO DBT.CONNECTFASTER.Persons_bkp select * from DBT.CONNECTFASTER.Persons
+-- limit 1;
 -- Created backup table
 /*CREATE TABLE DBT.CONNECTFASTER.Persons_bkp (
     PersonID int,
