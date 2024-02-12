2aa4908c6408557d38c572f4f1a37a399cc877e0
Harish Pabbisetty <pabbisetty.hm@gmail.com>
2024-02-12 18:02:01 +0000

    r

--- models/staging/PERSONS_RAW_stg.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='view', transient=true) }}
-
-select *
-from persons_raw
-where status = 'NOTPROCESSED'
--- models/staging/PERSONS_STAGE.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='incremental', unique_key='PERSONID') }}
-select *
-from {{ ref('PERSONS_RAW_stg') }}
